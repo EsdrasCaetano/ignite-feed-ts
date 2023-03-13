ab91791618983604ff5dc09c907a9d226e863b62
unknown <esdrascaetano7@gmail.com>
2023-03-13 15:44:36 -0300

    final version

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import { Avatar } from './Avatar';
+import { PencilSimpleLine }  from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar(){
+    return (
+        <aside className={styles.sidebar}>
+           <img 
+            className={styles.cover}
+            src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=50" 
+           />
+
+           <div className={styles.profile}>
+                <Avatar src="https://github.com/esdrascaetano.png" />
+                <strong>Deda</strong>
+                <span>Web Developer</span>
+           </div>
+
+           <footer>
+                <a href="">
+                    <PencilSimpleLine />
+                    Editar seu perfil
+                </a>
+           </footer>
+        </aside>
+    )
+}
